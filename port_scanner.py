14449716fcf1512d40d7d1d1e9bd23ea546cf411
Rootingg <91620858+Rootingg@users.noreply.github.com>
2022-12-05 17:09:39 +0100

    Add files via upload

---
+++ port_scanner.py
@@ -0,0 +1,19 @@
+#PORT SCANNER
+import socket
+
+target = input("IP Adress to scan: ")
+portrange = input("Enter port range to scan: ")
+
+lowport = int(portrange.split('-')[0])
+highport = int(portrange.split('-')[1])
+
+print("Scanning host ", target, "from port ",lowport, "to port", highport)
+
+for port in range(lowport,highport):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    status = s.connect_ex((target, port))
+    if (status == 0):
+        print("*** PORT ",port, '-OPEN ***')
+    else:
+        print("Port",port,"- CLOSED")
+    s.close()
